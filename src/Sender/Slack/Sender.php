4b2235f06ce632e054b443638ada68df909e7f6c
Andrey Demchenko <ehot@divandesign.ru>
2017-02-05 19:03:48 +0500

    + Added support for sending in to the slack.

---
+++ src/Sender/Slack/Sender.php
@@ -0,0 +1,53 @@
+<?php
+namespace ddSendFeedback\Sender\Slack;
+
+class Sender extends \ddSendFeedback\Sender\Sender {
+	private
+		$url = '',
+		$channel = '',
+		$botName = 'ddSendFeedback',
+		$botIcon = ':ghost:';
+	
+	public function __construct($params = []){
+		//Call base constructor
+		parent::__construct($params);
+		$this->url = $params['url'];
+		$this->channel = $params['channel'];
+	}
+	
+	/**
+	 * send
+	 * @version 1.0 (2017-01-16)
+	 * 
+	 * @uses ddMakeHttpRequest >= 1.3.
+	 * 
+	 * @desc Send message to Slack.
+	 * 
+	 * @return $result {array} — Returns the array of send status.
+	 * @return $result[0] {0|1} — Status.
+	 */
+
+	public function send(){
+		global $modx;
+		$result = [0 => 0];
+		
+		$requestResult = $modx->runSnippet('ddMakeHttpRequest', [
+			'url' => $this->url,
+			'method' => 'post',
+			'postData' => json_encode([
+				'text' => $this->text,
+				'channel' => $this->channel,
+				'username' => $this->botName,
+				'icon_emoji' => $this->botIcon
+			]),
+			'headers' => 'application/json'
+		]);
+		
+		if ($requestResult == 'ok'){
+			$result[0] = 1;
+		}
+		
+		return $result;
+	}
+}
+?>
