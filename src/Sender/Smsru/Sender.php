5d953a364fbb1f45ccb89dd8b8669634b348d61a
Ilyas Ronef <ilyas@DivanDesign.ru>
2024-06-17 14:36:56 +0400

    * `\ddSendFeedback\Sender\Smsru\Sender::send`: Refactoring.

--- src/Sender/Smsru/Sender.php
+++ src/Sender/Smsru/Sender.php
@@ -15,7 +15,7 @@ class Sender extends \ddSendFeedback\Sender\Sender {
 		$requestResultParams = [
 			'checkValue' => 'OK',
 			'isCheckTypeSuccess' => true,
-			//Just something, a real values will be set in every call of `$this->send`
+			//Just something, a real values will be set in every call of `$this->send_request_prepareParams`
 			'checkPropName' => 'sms.[+phoneNumber+].status',
 			'errorMessagePropName' => 'sms.[+phoneNumber+].status_text',
 			
@@ -28,92 +28,15 @@ class Sender extends \ddSendFeedback\Sender\Sender {
 	;
 	
 	/**
-	 * send
-	 * @version 1.3.3 (2024-06-17)
-	 * 
-	 * @desc Send sms via sms.ru.
-	 * 
-	 * @return $result {array} — Returns the array of send status.
-	 * @return $result[0] {0|1} — Status.
-	 */
-	public function send(){
-		$errorData = (object) [
-			'isError' => true,
-			//Only 19 signs are allowed here in MODX event log :|
-			'title' => 'Check required parameters',
-			'message' => '',
-		];
-		
-		$requestResult = null;
-		
-		if ($this->canSend){
-			$errorData->title = 'Unexpected API error';
-			
-			$this->requestResultParams->checkPropName = 'sms.' . $this->to . '.status';
-			$this->requestResultParams->errorMessagePropName = 'sms.' . $this->to . '.status_text';
-			
-			$requestResult = $this->send_parseRequestResult(
-				$this->send_request()
-			);
-			
-			$errorData->isError = $requestResult->isError;
-		}
-		
-		//Log errors
-		if ($errorData->isError){
-			if (!is_null($requestResult)){
-				if (!\ddTools::isEmpty($this->requestResultParams->errorMessagePropName)){
-					//Try to get error title from request result
-					$errorData->title = \DDTools\ObjectTools::getPropValue([
-						'object' => $requestResult->data,
-						'propName' => $this->requestResultParams->errorMessagePropName,
-						'notFoundResult' => $errorData->title,
-					]);
-				}
-				
-				$errorData->message =
-					'<p>Request result:</p><pre><code>'
-						. var_export(
-							$requestResult->data,
-							true
-						)
-					. '</code></pre>'
-				;
-			}
-			
-			$errorData->message .=
-				'<p>$this:</p><pre><code>'
-					. var_export(
-						$this,
-						true
-					)
-				. '</code></pre>'
-			;
-			
-			\ddTools::logEvent([
-				'message' => $errorData->message,
-				'source' =>
-					'ddSendFeedback → '
-					. static::getClassName()->namespaceShort
-					. ': '
-					. $errorData->title
-				,
-				'eventType' => 'error',
-			]);
-		}
-		
-		return [
-			0 => !$errorData->isError
-		];
-	}
-	
-	/**
 	 * send_request_prepareParams
-	 * @version 1.0.1 (2024-06-11)
+	 * @version 1.1 (2024-06-17)
 	 * 
 	 * @return $result {\stdClass}
 	 */
 	protected function send_request_prepareParams(): \stdClass {
+		$this->requestResultParams->checkPropName = 'sms.' . $this->to . '.status';
+		$this->requestResultParams->errorMessagePropName = 'sms.' . $this->to . '.status_text';
+		
 		$url =
 			$this->url .
 			'&api_id=' . $this->apiId .
